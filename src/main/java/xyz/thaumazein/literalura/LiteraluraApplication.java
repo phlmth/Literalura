d4b15f2cd6e129c6eaa1d28135c9e06b52774452
Jose P. <shzexyz@gmail.com>
2025-07-20 10:15:41 -0300

    sync

---
+++ src/main/java/xyz/thaumazein/literalura/LiteraluraApplication.java
@@ -0,0 +1,28 @@
+package xyz.thaumazein.literalura;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import xyz.thaumazein.literalura.repository.AuthorRepository;
+import xyz.thaumazein.literalura.repository.BookRepository;
+
+@SpringBootApplication
+public class LiteraluraApplication implements CommandLineRunner {
+
+	@Autowired
+	private AuthorRepository authorRepository;
+
+	@Autowired
+	private BookRepository bookRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraluraApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Main main = new Main(bookRepository, authorRepository);
+		main.run();
+	}
+}
